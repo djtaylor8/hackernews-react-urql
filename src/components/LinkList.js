4cd1a54741e5e018fc1393ce4486b0ce01146a89
DJ <dtaylor88@gmail.com>
2021-11-16 14:31:37 -0800

    add index to each link with upvote button

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -11,6 +11,16 @@ const FEED_QUERY = gql `
                 createdAt
                 url
                 description
+                postedBy {
+                    id
+                    name
+                }
+                votes {
+                    id
+                    user {
+                        id
+                    }
+                }
             }
         }
     }
@@ -28,7 +38,7 @@ const LinkList = () => {
 
     return (
         <div>
-           {linksToRender.map(link => <Link key={link.id} link={link} />)} 
+           {linksToRender.map((link, index) => <Link key={link.id} index={index} link={link} />)} 
         </div>
     );
 };
