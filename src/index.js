f64078dd92045955704ce53b604a83338c6ae487
DJ <dtaylor88@gmail.com>
2021-11-15 17:53:11 -0800

    add database, set up urql client

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
+import { Provider, Client, dedupExchange, fetchExchange } from 'urql';
+import { cacheExchange } from '@urql/exchange-graphcache';
+
+const cache = cacheExchange({})
+
+const client = new Client({
+  url: 'http://localhost:4000',
+  exchanges: [dedupExchange, cache, fetchExchange],
+})
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider value={client}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
-);
-
+)
