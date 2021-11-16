d340139a598ba5a210511fb7e021f12661c922c1
DJ <dtaylor88@gmail.com>
2021-11-15 19:41:30 -0800

    update switch to routes for router

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import '../styles/App.css';
+
+import Header from './Header';
 import CreateLink from './CreateLink';
 import LinkList from './LinkList';
 
 function App() {
   return (
-    <div>
-      <CreateLink />
+    <div className='center w85'>
+      <Header/>
+      <div className='ph3 pv1 background-gray'>
+        <Routes>
+          <Route exact path='/' element={<LinkList />} />
+          <Route exact path='/create' element={<CreateLink />} />
+        </Routes>
+      </div>
     </div>
   );
 }
