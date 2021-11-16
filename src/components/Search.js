3b8404d03b6d1b7eb916083f6502a786bbe59445
DJ <dtaylor88@gmail.com>
2021-11-16 14:56:23 -0800

    add search query using execute query

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,16 +3,47 @@ import { useQuery } from 'urql';
 import gql from 'graphql-tag';
 import Link from './Link';
 
+const FEED_SEARCH_QUERY = gql`
+    query FeedSearchQuery($filter: String!) {
+        feed(filter: $filter) {
+            links {
+                id
+                url
+                description
+                createdAt
+                postedBy {
+                    id
+                    name
+                }
+                votes {
+                    id
+                    user {
+                        id
+                    }
+                }
+            }
+        }
+    }
+`
+
 const Search = () => {
     const [filter, setFilter] = React.useState('');
 
-    const search = React.useCallback(() => {}, []);
-    const links = [];
+    const [result, executeQuery] = useQuery({
+        query: FEED_SEARCH_QUERY,
+        variables: { filter },
+        pause: true,
+    });
+
+    const search = React.useCallback(() => {
+        executeQuery();
+    }, [executeQuery]);
+
+    const links = result.data ? result.data.feed.links : [];
 
     return (
         <div>
             <div>
-                Search
                 <input 
                     type='text'
                     onChange={e => setFilter(e.target.value)}
